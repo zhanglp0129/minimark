4ed33199d157348e2f3f0a7c4fb747710d91b0b1
zhanglp0129 <zhanglp0129@gmail.com>
2024-06-21 09:12:03 +0800

    修复了删除分类时，该分类下存在商品的bug

--- service/category.go
+++ service/category.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"gorm.io/gorm"
 	"minimark/dao"
 )
 
@@ -32,6 +33,20 @@ func CategoryUpdate(category *dao.Category) error {
 
 func CategoryDelete(id int) error {
 	db := dao.GetDB()
-	err := db.Delete(&dao.Category{}, id).Error
+	err := db.Transaction(func(tx *gorm.DB) error {
+		// 先将商品的分类置为null
+		err := tx.Model(&dao.Goods{}).Where("category_id = ?", id).Update("category_id", nil).Error
+		if err != nil {
+			return err
+		}
+
+		// 再删除商品分类
+		err = tx.Delete(&dao.Category{ID: id}).Error
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 	return err
 }
